ee1060801075c3e0f69910cfa53e6e26412d8440
Junior Matelau <matelau@gmail.com>
2017-10-21 18:07:32 -0600

    added state to crawler

--- web/src/Crawler.js
+++ web/src/Crawler.js
@@ -1,19 +1,47 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Crawler extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+      search: '',
+    };
+  }
   render() {
     return (
       <div class="Crawler-search">
-        Craigslist
-        <div class="Crawler-search-box">
-          <form>
-            <textarea class="Crawler-search-text">Search</textarea>
-            <button class="Crawler-search-submit">Crawl</button>
-          </form>
+        <span>
+          Craigslist
+          <div class="Crawler-search-box">
+            <input
+              type="text"
+              placeholder="Search"
+              value={this.state.search}
+              onChange={evt => this.updateInputValue(evt)}
+              class="Crawler-search-text"
+            />
+            <button class="Crawler-search-submit" onClick={() => this.search()}>
+              Crawl
+            </button>
+          </div>
+        </span>
+        <div class="Crawler-results">
+          <li>{this.state.results}</li>
         </div>
       </div>
     );
   }
+  search() {
+    this.setState({
+      results: [...this.state.results, this.state.search],
+    });
+  }
+  updateInputValue(evt) {
+    console.log(evt.target.value);
+    this.setState({
+      search: evt.target.value,
+    });
+  }
 }
-
 export default Crawler;
